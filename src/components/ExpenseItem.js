11cff0d1e4b3f042091d78378d06ba2c530fb4c9
psywa1 <psywa1@exmail.nottingham.ac.uk>
2021-07-16 15:38:54 +0100

    Styled ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,15 @@
-function ExpenseItem()
-{
-    return (
-        <h2>Expense Item lmao</h2>
-    );
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>June 6th 2021</div>
+      <div className="expense-item__description">
+        <h2>Flowers</h2>
+        <div className="expense-item__price">£120.40</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
