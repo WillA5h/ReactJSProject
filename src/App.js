ddad496ba3d3da974bd9d4961430db3ea6a0d988
psywa1 <psywa1@exmail.nottingham.ac.uk>
2021-07-19 11:50:04 +0100

    changable data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Flowers',
+      amount: '35.23',
+      date: new Date (2021, 6, 6),
+    },
+    {
+      id :'e2',
+      title: 'GPU',
+      amount: '1000.99',
+      date: new Date (2020, 1, 25),
+    },
+    {
+      id: 'e3',
+      title: 'Nespresso Pods',
+      amount: '25.00',
+      date: new Date (2021, 4, 12),
+    }
+  ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title = {expenses[0].title}
+        amount = {expenses[0].amount}
+        date = {expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title = {expenses[1].title}
+        amount = {expenses[1].amount}
+        date = {expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title = {expenses[2].title}
+        amount = {expenses[2].amount}
+        date = {expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
