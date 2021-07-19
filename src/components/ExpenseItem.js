9befc9c4ed984f86ba9ef2544b9cf0ef57b3b87b
psywa1 <psywa1@exmail.nottingham.ac.uk>
2021-07-19 12:51:48 +0100

    date easier to read

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const expenseMonth = props.date.toLocaleString('en-US', {month : 'long'});
+    const expenseDay = props.date.toLocaleString('en-US', {day : '2-digit'});
+    const expenseYear = props.date.getFullYear();
+
     return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{expenseDay}</div>
+        <div>{expenseMonth}</div>  
+        <div>{expenseYear}</div>  
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
